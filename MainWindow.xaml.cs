3dbd3c6f97707d2f94791a09b994fb6230ef9ec9
浩劫者12345 <3112611479@qq.com>
2023-10-20 02:00:06 +0800

    added mousewheel, click, adjust interval

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,12 +27,17 @@ namespace MouseRefreshRateTest
         private void Window_Loaded(object sender, EventArgs e)
         {
             show_fps = new Thread(() => {
+                int interval = 250;
                 while (true)
                 {
-                    Thread.Sleep(250);
+                    Thread.Sleep(interval);
                     Application.Current.Dispatcher.Invoke(() =>
                     {
                         label1.Content = count * 1000 / (DateTimeOffset.Now.ToUnixTimeMilliseconds() - time) + "/s";
+                        try
+                        {
+                            interval = int.Parse(intervalms.Text);
+                        } catch { }
                     });
                     count = 0;
                     time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
@@ -80,6 +85,34 @@ namespace MouseRefreshRateTest
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             show_fps.Abort();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox.SelectedIndex == 0) {
+                win.MouseMove += count_plus;
+                win.MouseWheel -= count_plus;
+                win.MouseDown -= count_plus;
+            }
+            else if(comboBox.SelectedIndex == 1)
+            {
+                win.MouseMove -= count_plus;
+                win.MouseWheel += count_plus;
+                win.MouseDown -= count_plus;
+            }
+            else if (comboBox.SelectedIndex == 2)
+            {
+                win.MouseMove -= count_plus;
+                win.MouseWheel -= count_plus;
+                win.MouseDown += count_plus;
+            }
+        }
+
+        private void removeHistogram(object sender, RoutedEventArgs e)
+        {
+            histogram.Children.RemoveRange(0, histogram.Children.Count);
+            histogramTranslate.X = 0;
         }
     }
 }
